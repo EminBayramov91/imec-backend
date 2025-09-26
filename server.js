d5b4f6e6328eec419dd710802a0964c5416b0be0
Эмин Байрамов <eminbayramov1991@gmail.com>
2025-09-26 12:31:08 +0400

    server.js app listen edit

--- server.js
+++ server.js
@@ -110,8 +110,8 @@ app.post('/api/contact', async (req, res) => {
 
 initMailer()
     .then(() => {
-        app.listen(PORT, () => {
-            console.log(`🚀 Server running on http://localhost:${PORT}`);
+        app.listen(PORT, "0.0.0.0", () => {
+            console.log(`🚀 Server running on port ${PORT} (accessible from outside)`);
         });
     })
     .catch((err) => {
